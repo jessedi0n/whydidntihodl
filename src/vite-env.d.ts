22b5bc6a9e971f12440315538151ad910d793971
Jesse <jesse.mahnken00@gmail.com>
2024-11-26 21:04:21 +0100

    configure github pages deployment

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,14 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly PROD: boolean
+  readonly DEV: boolean
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
   const component: DefineComponent<{}, {}, any>;
